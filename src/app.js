e6951ed52d4f0ae864ad81b5958114088718fc2a
NZXT-HYBRID\NZXT-Hybrid <stalfos1211@gmail.com>
2017-08-12 17:38:04 -0500

    don't know

---
+++ src/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let express = require('express'),
+    posts = require('./mock/post.json'),
+    jade = require('jade')
+
+const app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
+app.get('/', (req, res) => {
+  res.render('index');
+  //res.send('I Love Treehouse!');
+})
+
+app.get('/blog/:title?', (req, res) => {
+  let title = req.params.title
+  if (title === undefined) {
+    res.status(503);
+    res.send('this page is under construction');
+  }
+  else {
+    var post = posts['post' + title];
+    //res.send(post)
+    res.render('post', {post: post});
+  }
+})
+
+app.listen(8000);
+
